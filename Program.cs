c678db2ebc3883298dd95fda58a7f5d2f282400a
solomax <solomax2014@users.noreply.github.com>
2022-06-19 21:49:19 +0300

    Added semi-working solution for bonus task

--- Program.cs
+++ Program.cs
@@ -5,14 +5,19 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Io;
 
 namespace SiteParseTestTask {
     public class Worker {
-        private static IBrowsingContext context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+        private static HttpClient httpClient = new HttpClient();
+        private static IBrowsingContext context = BrowsingContext.New();
+        private static string cityId = "61000001000";
 
         public static void Main(string[] args) => new Worker().AsyncWorker().GetAwaiter().GetResult();
 
@@ -24,13 +29,14 @@ namespace SiteParseTestTask {
             while (true) {
                 page++;
                 Console.WriteLine($"    Страница {page}");
-                var doc = await context.OpenAsync(pageUrl);
+                var html = await GetCookedResp(pageUrl);
+                var doc = await context.OpenAsync(x => x.Content(html));
                 var productUrls = doc.QuerySelectorAll("div.row.mt-2 > div > div.product-card > div > div > a.product-name.gtm-click").Select(x => $"https://www.toy.ru{x.GetAttribute("href")}").ToList();
                 //productUrls.ForEach(x => Console.WriteLine(x));
                 productList.AddRange(productUrls);
 
-                var nextPageEl = doc.QuerySelectorAll("ul.pagination > li > a").Where(x => x.TextContent == "След.");
-                pageUrl = nextPageEl.Any() ? nextPageEl.Select(x => $"https://www.toy.ru{x.GetAttribute("href")}").First() : null;
+                var nextPageEl = doc.QuerySelectorAll("ul.pagination > li > a").Last();
+                pageUrl = nextPageEl.TextContent.EndsWith('.') ? $"https://www.toy.ru{nextPageEl.GetAttribute("href")}" : null;
                 if (pageUrl == null)
                     break;
             }
@@ -52,7 +58,8 @@ namespace SiteParseTestTask {
 
         private async Task<string> ProcessProduct(string url) {
             Console.WriteLine($"Обрабатываем {url}");
-            var doc = await context.OpenAsync(url);
+            var html = await GetCookedResp(url);
+            var doc = await context.OpenAsync(x => x.Content(html));
             var region = doc.QuerySelector("div.select-city-link > a")?.TextContent.Trim();
             var breadcrumbs = String.Join(" > ", doc.QuerySelector("nav.breadcrumb")?.Children.Where(x => x.TextContent != "Вернуться").Select(x => x.TextContent).ToList()).Replace(',', ';');
             var title = doc.QuerySelector("h1.detail-name")?.TextContent.Replace(',', ';');
@@ -63,6 +70,14 @@ namespace SiteParseTestTask {
             var csvRow = $"{region},{breadcrumbs},{title},{price},{oldPrice},{available},{images},{url}";
             //Console.WriteLine(csvRow);
             return csvRow;
+        }
+
+        public static async Task<string> GetCookedResp(string url) {
+            var req = new HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+            req.Headers.Add("Cookie", $"BITRIX_SM_country=; BITRIX_SM_country_zip=; BITRIX_SM_country_name=; BITRIX_SM_country_city=; BITRIX_SM_country_fias=; BITRIX_SM_city={cityId}");
+            var resp = await httpClient.SendAsync(req);
+            var str = await resp.Content.ReadAsStringAsync();
+            return str;
         }
     }
 }
